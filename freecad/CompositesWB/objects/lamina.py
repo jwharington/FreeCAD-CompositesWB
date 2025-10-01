d8920152e4581c0ce8e11ddfac2ae035769af556
John Wharington <jwharington@gmail.com>
2025-10-02 04:04:13 +1000

    move missing child props

--- freecad/CompositesWB/objects/lamina.py
+++ freecad/CompositesWB/objects/lamina.py
@@ -5,3 +5,11 @@ from dataclasses import dataclass
 class Lamina:
     core: bool = False
     thickness: float = 1
+
+    @staticmethod
+    def set_missing_child_props(parent, children, items):
+        for la in children:
+            for item in items:
+                if hasattr(la, item) and not getattr(la, item):
+                    value = getattr(parent, item)
+                    setattr(la, item, value)
--- freecad/CompositesWB/objects/ply.py
+++ freecad/CompositesWB/objects/ply.py
@@ -5,11 +5,3 @@ from .lamina import Lamina
 @dataclass  # (frozen=True)
 class Ply(Lamina):
     orientation: float = 0
-
-    @staticmethod
-    def set_missing_child_props(parent, children, items):
-        for la in children:
-            for item in items:
-                if hasattr(la, item) and not getattr(la, item):
-                    value = getattr(parent, item)
-                    setattr(la, item, value)
