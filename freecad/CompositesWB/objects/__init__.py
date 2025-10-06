6a1d28eb2a110195e67682935bff2401174eead9
John Wharington <jwharington@gmail.com>
2025-10-07 02:00:20 +1100

    refactor

--- freecad/CompositesWB/objects/__init__.py
+++ freecad/CompositesWB/objects/__init__.py
@@ -2,5 +2,6 @@ from .symmetry_type import SymmetryType  # noqa
 from .weave_type import WeaveType  # noqa
 from .simple_fabric import SimpleFabric  # noqa
 from .homogeneous_lamina import HomogeneousLamina  # noqa
+from .laminate import Laminate  # noqa
 from .composite_laminate import CompositeLaminate  # noqa
 from .fibre_composite_lamina import FibreCompositeLamina  # noqa
