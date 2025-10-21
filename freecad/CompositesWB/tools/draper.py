3f7d496a21bcee28bcc868bc25607340ba30a9f5
John Wharington <jwharington@gmail.com>
2025-10-21 17:15:51 +1100

    draper: documentation

--- freecad/CompositesWB/tools/draper.py
+++ freecad/CompositesWB/tools/draper.py
@@ -40,7 +40,22 @@ class Draper:
             return
 
         self.fabric_points = [Vector(*n) for n in self.flattener.ze_nodes]
-        self.T_fo = self.calc_flat_rotation()
+
+        def calc_flat_rotation():
+            lcs = self.lcs.getGlobalPlacement()
+
+            T_lcs = lcs.Rotation.inverted()
+            center = T_lcs * lcs.Base
+            tri_global, tri_fabric = self._get_facet(center)
+            tri_global = [T_lcs * p for p in tri_global]
+            lam = calc_lambda_vec(center, tri_global)
+
+            q = axes_mapped(lam, tri_fabric, tri_global)
+            R = Rotation(q[0], q[1], Vector(0, 0, 1), "ZXY").inverted()
+            origin = Vector(eval_lam(lam, tri_fabric))
+            return Base.Placement(-origin, R, origin)
+
+        self.T_fo = calc_flat_rotation()
         self.fabric_points = [self.T_fo * p for p in self.fabric_points]
 
         # for i, tri in enumerate(mesh.Topology[1]):
@@ -50,38 +65,8 @@ class Draper:
     def isValid(self):
         return self.flattener
 
-    def calc_flat_rotation(self):
-        lcs = self.lcs.getGlobalPlacement()
-
-        T_lcs = lcs.Rotation.inverted()
-        center = T_lcs * lcs.Base
-        tri_global, tri_fabric = self._get_facet(center)
-        tri_global = [T_lcs * p for p in tri_global]
-        lam = calc_lambda_vec(center, tri_global)
-
-        q = axes_mapped(lam, tri_fabric, tri_global)
-        R = Rotation(q[0], q[1], Vector(0, 0, 1), "ZXY").inverted()
-        origin = Vector(eval_lam(lam, tri_fabric))
-        return Base.Placement(-origin, R, origin)
-
-    def get_uv(self, p):
-        dmin = None
-        pint = None
-        fmin: Part.Face = None
-        vert = Part.Vertex(p.x, p.y, p.z)
-        for f in self.shape.Faces:
-            distance, points, info = f.distToShape(vert)
-            if (not fmin) or (distance < dmin):
-                dmin = distance
-                pint = points[0][0]
-                fmin = f
-        return (fmin.Surface.parameter(pint), fmin)
-
-    def get_normal_projected(self, point):
-        ((u, v), surface) = self.get_uv(point)
-        return surface.valueAt(u, v), surface.normalAt(u, v)
-
-    def get_tris(self, i):
+    # internal use only
+    def _get_tris(self, i):
         simp = self.mesh.Topology[1][i]
         tri_global = [self.mesh.Points[i].Vector for i in simp]
         tri_fabric = [self.fabric_points[i] for i in simp]
@@ -95,7 +80,7 @@ class Draper:
 
         totd = [tri_dist(tri) for tri in self.mesh.Topology[1]]
         facet = np.argmin(totd)
-        return self.get_tris(facet)
+        return self._get_tris(facet)
 
     def _get_lcs_at_point(self, center: Vector, normal: Vector):
         tri_global, tri_fabric = self._get_facet(center)
@@ -104,26 +89,51 @@ class Draper:
         d = axes_mapped(lam, tri_global, tri_fabric)
         return Rotation(d[0], d[1], normal, "ZXY").inverted()
 
-    def get_lcs_at_point(self, center: Vector):
-        p, normal = self.get_normal_projected(center)
-        return self._get_lcs_at_point(p, normal)
-
+    # use by FEM given fem triangles
     def get_lcs(self, tri):
         center = (tri[0] + tri[1] + tri[2]) / 3
         normal = (tri[1] - tri[0]).cross(tri[2] - tri[1]).normalize()
         return self._get_lcs_at_point(center, normal)
 
-    def get_tex_coords(self, offset_angle_deg):
-        # save texture coordinates for rendering pattern in 3d
-        T = z_rotation(offset_angle_deg)
-        return [T * p for p in self.fabric_points]
+    # use by LCS transfer tools
+    def get_lcs_at_point(self, center: Vector):
 
+        def get_uv(p):
+            dmin = None
+            pint = None
+            fmin: Part.Face = None
+            vert = Part.Vertex(p.x, p.y, p.z)
+            for f in self.shape.Faces:
+                distance, points, info = f.distToShape(vert)
+                if (not fmin) or (distance < dmin):
+                    dmin = distance
+                    pint = points[0][0]
+                    fmin = f
+            return (fmin.Surface.parameter(pint), fmin)
+
+        def get_normal_projected(point):
+            ((u, v), surface) = get_uv(point)
+            return surface.valueAt(u, v), surface.normalAt(u, v)
+
+        p, normal = get_normal_projected(center)
+        return self._get_lcs_at_point(p, normal)
+
+    # use by external alignment tools
     def get_tex_coord_at_point(self, point, offset_angle_deg=0):
         # save texture coordinates for rendering pattern in 3d
         tri_global, tri_fabric = self._get_facet(point)
         lam = calc_lambda_vec(point, tri_global)
         return z_rotation(offset_angle_deg) * eval_lam(lam, tri_fabric)
 
+    # operations across whole mesh
+
+    # use by grid rendering
+    def get_tex_coords(self, offset_angle_deg=0):
+        # save texture coordinates for rendering pattern in 3d
+        T = z_rotation(offset_angle_deg)
+        return [T * p for p in self.fabric_points]
+
+    # use by texture plan
     def get_boundaries(self, offset_angle_deg):
         T = self.T_fo * z_rotation(offset_angle_deg)
         wires = []
@@ -133,10 +143,11 @@ class Draper:
             wires.append(points)
         return wires
 
+    # use by ? texture plan analysis or grid rendering
     def calc_strain(self, facet):
         # https://www.ce.memphis.edu/7117/notes/presentations/chapter_06a.pdf
 
-        G, F = self.get_tris(facet)
+        G, F = self._get_tris(facet)
         R = self.get_lcs(G)
         G = [R * g for g in G]
         D = [g - f for f, g in zip(F, G)]
@@ -152,12 +163,3 @@ class Draper:
         eyy = gamma.dot(v)
         exy = gamma.dot(u) + beta.dot(v)
         return np.array([exx, eyy, exy]) / two_area
-
-
-# precompute and store:
-# - normal in global coords for all triangles
-# - area of each triangle
-#
-#  mesh: find nearest simplex
-#           calc barycentric coordinates
-#  mesh.nearestFacetOnRay()
