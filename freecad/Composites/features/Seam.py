1180d16e7686f5cbc5ba7404997413893d7b25ee
John Wharington <jwharington@gmail.com>
2025-10-23 00:55:21 +1100

    seam new approach

--- freecad/Composites/features/Seam.py
+++ freecad/Composites/features/Seam.py
@@ -1,44 +1,105 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 # Copyright 2025 John Wharington jwharington@gmail.com
 
-from FreeCAD import Console
-import Part
 import FreeCADGui
 from .. import (
     SEAM_TOOL_ICON,
 )
-from ..tools.seam import make_seam
+from ..tools.seam import (
+    make_join_seam,
+    make_edge_seam,
+)
+from .Command import BaseCommand
 
 
-class CompositeSeamCommand:
-    """Composite seam command"""
+class SeamFP:
+    def __init__(self, obj):
 
-    def GetResources(self):
-        return {
-            "Pixmap": SEAM_TOOL_ICON,
-            "MenuText": "Seam",
-            "ToolTip": "Generate seam edge.  WORK-IN-PROGRESS",
-        }
+        obj.addProperty(
+            "App::PropertyLinkSub",
+            "SourceA",
+            "References",
+            "Link to the shape",
+            locked=True,
+        ).SourceA = None
 
-    def check_sel(self):
-        sel = FreeCADGui.Selection.getSelectionEx()
-        if len(sel) == 1:
-            sel = [sel[0].SubObjects[0], sel[0].SubObjects[1]]
-        elif len(sel) == 2:
-            sel = [sel[0].SubObjects[0], sel[1].SubObjects[0]]
-        else:
-            return None
-        return sel
+        obj.addProperty(
+            "App::PropertyLinkSubList",
+            "Edges",
+            "References",
+            "Edges",
+            locked=True,
+        ).Edges = []
 
-    def Activated(self):
-        if sel := self.check_sel():
-            Part.show(make_seam(sel[0], sel[1]))
-        else:
-            Console.PrintError("Select two faces\r\n")
+        obj.addProperty(
+            "App::PropertyLength",
+            "Overlap",
+            "Dimension",
+            "Overlap length",
+            locked=True,
+        ).Overlap = "10.0 mm"
+
+        obj.Proxy = self
+
+    def onChanged(self, fp, prop):
         return
 
-    def IsActive(self):
-        return self.check_sel() is not None
+    def execute(self, fp):
+
+        face = fp.SourceA[0].getSubObject(fp.SourceA[1])[0]
+        edges = [e[0].getSubObject(e[1])[0] for e in fp.Edges]
+
+        shape = make_edge_seam(
+            face=face,
+            edges=edges,
+            overlap=fp.Overlap,
+        )
+        fp.Shape = shape
+
+    def onDocumentRestored(self, fp):
+        fp.recompute()
+
+
+class ViewProviderSeam:
+    def __init__(self, obj):
+        self.obj = obj
+        obj.Proxy = self
+
+    def claimChildren(self):
+        return []  # [self.obj.Object.Source]
+
+    def __getstate__(self):
+        return None
+
+    def __setstate__(self, state):
+        return None
+
+
+class CompositeSeamCommand(BaseCommand):
+
+    icon = SEAM_TOOL_ICON
+    menu_text = "Seam"
+    tool_tip = "Generate seam edge. WORK-IN-PROGRESS"
+    sel_args = [
+        # {
+        #     "key": "source",
+        #     "type": "Part::Feature",
+        # },
+    ]
+    type_id = "Part::FeaturePython"
+    instance_name = "Seam"
+    cls_fp = SeamFP
+    cls_vp = ViewProviderSeam
 
 
 FreeCADGui.addCommand("Composites_Seam", CompositeSeamCommand())
+
+
+# sel = FreeCADGui.Selection.getSelectionEx()
+# if len(sel) == 1:
+#     sel = [sel[0].SubObjects[0], sel[0].SubObjects[1]]
+# elif len(sel) == 2:
+#     sel = [sel[0].SubObjects[0], sel[1].SubObjects[0]]
+# else:
+#     return None
+# return sel
--- freecad/Composites/tools/seam.py
+++ freecad/Composites/tools/seam.py
@@ -3,138 +3,53 @@
 
 
 import Part
-import numpy as np
-from .geom_utils import find_uv_intersection
 from . import splitAPI
 
 
-def get_partner_edges(face1, face2):
+def generate_seam_tube(wire: Part.Wire, overlap: float):
+    o = wire.Edges[0].firstVertex().Point
+    # wire.valueAt(wire.FirstParameter)
+
+    def make_section():
+        c = Part.Circle()
+        c.Center = o
+        c.Axis = (0, 1, 0)
+        c.Radius = overlap
+        return Part.Wire([c.toShape()])
+
+    makeSolid = True
+    isFrenet = True
+    return wire.makePipeShell([make_section()], makeSolid, isFrenet)
+
+
+def make_edge_seam(
+    face: Part.Face,
+    edges: list[Part.Edge],
+    overlap: float = 10,
+):
+    sedges = Part.__sortEdges__(edges)
+    tools = [generate_seam_tube(Part.Wire(e), overlap) for e in sedges]
+    return splitAPI.slice(face, tools, "Split", 1e-6)
+
+
+def get_partner_edges(
+    face1: Part.Face,
+    face2: Part.Face,
+):
     return [e2 for e2 in face2.Edges for e1 in face1.Edges if e2.isPartner(e1)]
 
 
-def get_seam_edge_point(surface, edge, t, overlap):
+def make_join_seam(
+    face1: Part.Face,
+    face2: Part.Face,
+    overlap: float = 10,
+):
+    edges = get_partner_edges(face1, face2)
 
-    p1 = edge.valueAt(t)
-    (u, v) = surface.parameter(p1)
-    surface_normal = surface.normal(u, v)
-    edge_tangent = edge.tangentAt(t)
-    n = edge_tangent.cross(surface_normal)
-    # n is vector to seam from p1
-
-    def project(side=1):
-        p2 = p1 + side * n * overlap
-        p3 = surface.projectPoint(p2)
-        if p3.distanceToPoint(p2) < 1.5 * overlap:
-            return p3
-        return None
-
-    for side in [1, -1]:
-        if p3 := project(side):
-            return p3
-
-    # alternate approach:
-    # - solve for point a given distance from reference point on edge
-
-    raise ValueError("invalid")
-
-
-def extend_seam_edge(surface, face, p, p_prev):
-    # find value for which hits border
-    uv = surface.parameter(p)
-    uv_prev = surface.parameter(p_prev)
-    try:
-        (u, v) = find_uv_intersection(uv, uv_prev, face)
-        p_new = face.valueAt(u, v)
-        t = p_new.distanceToPoint(p) / p.distanceToPoint(p_prev)
-        if t > 1:
-            p_new = p + (p_new - p) / t
-        if p_new.distanceToPoint(p) < 1.0e-3:
-            raise ValueError("no change")
-        return p_new
-    except ValueError:
-        return None
-
-
-def make_seam(face1, face2, overlap=10, num_points=64):
-
-    def generate_seam_line(surface, edge):
-
-        points = []
-
-        def add_unique_point(p):
-            if p not in points:
-                points.append(p)
-
-        # walk length of edge
-        trange = np.linspace(
-            edge.FirstParameter,
-            edge.LastParameter,
-            num_points,
-        )
-        for t in trange:
-            if point := get_seam_edge_point(
-                surface,
-                edge,
-                t,
-                overlap,
-            ):
-                add_unique_point(point)
-
-        # project last two points further
-        while (
-            point := extend_seam_edge(
-                surface,
-                face2,
-                points[-1],
-                points[-2],
-            )
-        ) is not None:
-            add_unique_point(point)
-
-        # project first two points further
-        while (
-            point := extend_seam_edge(
-                surface,
-                face2,
-                points[0],
-                points[1],
-            )
-        ) is not None:
-            if point not in points:
-                points.insert(0, point)
-
-        def make_tool(points):
-            curve = Part.BSplineCurve()
-            curve.interpolate(points)
-            edges = Part.sortEdges(
-                [
-                    curve.toShape(),
-                    Part.LineSegment(points[-1], points[0]).toShape(),
-                ]
-            )[0]
-            return Part.makeFilledFace(edges)
-
-        return make_tool(points)
-
-    # set up reference edge
-    border = get_partner_edges(face1, face2)
-
-    if not border:
+    if not edges:
         print("TODO: handle non-common edge")
         # fallback to calculating via intersection
         # TODO: modify face2 to split
-        border = face1.section(face2).Edges
+        edges = face1.section(face2).Edges
 
-    # create cutting tool
-    tools = [generate_seam_line(face2.Surface, edge) for edge in border]
-
-    # slice and return result
-    return splitAPI.slice(face2, tools, "Split", 1e-6)
-
-    # for f in bf.Faces:
-    #     if f.isPartOfDomain(u, v):
-    #         obj.Shape = f
-    #         return
-
-
-# use fillet, find edge line, copy, remove fillet
+    return make_edge_seam(face1, edges, overlap=overlap)
